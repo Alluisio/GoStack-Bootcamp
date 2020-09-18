77bb52bfe921626abb4387ab67bb96c3db17ca42
Alluisio Silva <alluisio.silva@ipdec.org>
2020-09-18 10:59:55 -0400

    Iniciando Front-end com ReactJS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,115 @@
 const express = require("express");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello world" });
+app.use(express.json());
+
+/**
+ * Métodos HTTP:
+ *
+ * GET: Buscar informações do back-end;
+ * POST: Criar informações no back-end;
+ * PUT/PATCH: Alterar informações no back-end;
+ * DELETE: Deletar informações no back-end;
+ */
+
+/**
+ * Tipos de parâmetros:
+ *
+ * Query Params: Filtros e paginação ( ? + nome_do_parametro // & = anexar mais parâmetros )
+ * Route Params: Identificar recursos para put/delete (url + :id)
+ * Request Body: Conteúdo no body do request ( JSON // POST)
+ *
+ */
+
+/**
+ * Middleware:
+ *
+ * Interceptador de requisições  que pode interromper totalmente a requisição ou alterar dados da requisição
+ *
+ */
+
+const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+
+  return next();
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid project ID." });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId)
+
+app.get("/projects", (request, response) => {
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
 });
 
-app.listen(3334, () => {
-  console.log("Back-end started!! Port 3334");
+app.post("/projects", (request, response) => {
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+app.put("/projects/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found." });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found" });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.listen(3333, () => {
+  console.log("Back-end started!! Rodando na porta 3333");
 });
