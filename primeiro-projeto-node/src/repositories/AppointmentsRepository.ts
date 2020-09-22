6934078f47075bfa86116bfdc71a802ed21b19f0
Alluisio Silva <alluisio.silva@ipdec.org>
2020-09-22 13:22:30 -0400

    Appointments aula 8 parte 2 -- atividade 08

---
+++ primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,18 @@
+import Appointment from '../models/Appointment';
+import { EntityRepository, Repository } from 'typeorm';
+
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    // const findAppoitment = this.appointments.find(appointment =>
+    //   isEqual(date, appointment.date),
+    // );
+
+    const findAppoitment = await this.findOne({
+      where: { date },
+    });
+    return findAppoitment || null;
+  }
+}
+
+export default AppointmentsRepository;
