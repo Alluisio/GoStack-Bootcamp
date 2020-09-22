c59ed4e63fede1bd39618b05d63b927c7b37e2bb
Alluisio Silva <alluisio.silva@ipdec.org>
2020-09-22 08:58:31 -0400

    configs para dev

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,13 +1,10 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: " Hello World " });
-});
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log(" 💣 Server started on port 3333! ");
+  console.log(' 💣 Server started on port 3333! ');
 });
-
-// TODO: lembrar de modificar script para build
