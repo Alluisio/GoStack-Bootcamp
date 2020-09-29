98ade0d7631c5e3787b7c2de3aaf177a325aba1b
Alluisio Silva <alluisio.silva@ipdec.org>
2020-09-29 11:44:34 -0400

    Componentes Prime

--- primeiro-projeto-com-react/src/App.tsx
+++ primeiro-projeto-com-react/src/App.tsx
@@ -4,6 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 const App: React.FC = () => (
   <>
     <BrowserRouter>
