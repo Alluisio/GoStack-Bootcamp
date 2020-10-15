e1dcb82beff83cf14344b4bb1687511aa226066f
Alluisio Silva <alluisio.silva@ipdec.org>
2020-10-15 17:02:26 -0400

    hooks de toast

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -4,13 +4,19 @@ import SingIn from './pages/SignIn';
 // import SingUp from './pages/SingUp';
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/auth';
+import { ToastProvider } from './hooks/toast';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
-      <SingIn />
+      <ToastContainer>
+        <SingIn />
+      </ToastContainer>
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
---
+++ gobarber-web/src/hooks/index.tsx
@@ -0,0 +1,1 @@
+//TODO: Aula 10 -- atividade 3 -- minuto 5
