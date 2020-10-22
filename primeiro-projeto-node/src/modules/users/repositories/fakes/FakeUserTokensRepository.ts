70234c634f0d8f82c71fa3b4fa13bea946988276
Alluisio Silva <alluisio.silva@ipdec.org>
2020-10-22 15:24:24 -0400

    emails e template de emails finalizado

--- primeiro-projeto-node/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ primeiro-projeto-node/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -3,6 +3,7 @@ import { uuid } from 'uuidv4';
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
 import UserToken from '../../infra/typeorm/entities/UserToken';
+import User from '@modules/users/infra/typeorm/entities/User';
 
 class FakeUserTokensRepository implements IUserTokensRepository {
   private userTokens: UserToken[] = [];
@@ -14,9 +15,19 @@ class FakeUserTokensRepository implements IUserTokensRepository {
       id: uuid(),
       token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
 
     return userToken;
   }
