a946941491960e97fbfda4b32bdc114fd8793b51
Alluisio Silva <alluisio.silva@ipdec.org>
2020-12-17 11:29:25 -0400

    projeto do app gobarber

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,11 +1,9 @@
-import 'react-native-gesture-handler';
-
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 
 const App: React.FC = () => (
   <>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <StatusBar backgroundColor="#312e38" barStyle="light-content" />
     <View style={{ flex: 1, backgroundColor: '#312e38' }} />
   </>
 );
